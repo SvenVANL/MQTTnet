4858d684e6a427f1392a83b599ed782d46513934
SvenVANL <svanderpost@vanl.nl>
2024-01-19 12:34:16 +0100

    Copied changes in Server_ASP_NET.CS to new file Server_ASP_NET_VANL.CS and reverted changes to the original file

---
+++ Samples/Server/Server_ASP_NET_VANL.cs
@@ -0,0 +1,146 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable InconsistentNaming
+// ReSharper disable EmptyConstructor
+// ReSharper disable MemberCanBeMadeStatic.Local
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MQTTnet.AspNetCore;
+using MQTTnet.Server;
+
+namespace MQTTnet.Samples.Server;
+
+public static class Server_ASP_NET_VANL
+{
+    public static Task Start_Server_With_WebSockets_Support()
+    {
+        /*
+         * This sample starts a minimal ASP.NET Webserver including a hosted MQTT server.
+         */
+        var host = Host.CreateDefaultBuilder(Array.Empty<string>())
+            .ConfigureWebHostDefaults(
+                webBuilder =>
+                {
+                    webBuilder.UseKestrel(
+                        o =>
+                        {
+                            // This will allow MQTT connections based on TCP port 1883.
+                            o.ListenAnyIP(5001, l => l.UseMqtt());
+
+                            o.Listen(new System.Net.IPAddress(new byte[] {10, 10, 4, 163}), 5001, l => l.UseMqtt());
+
+                            // This will allow MQTT connections based on HTTP WebSockets with URI "localhost:5000/mqtt"
+                            // See code below for URI configuration.
+                            o.Listen(new System.Net.IPAddress(new byte[] {10, 10, 4, 163}), 9001); // Default HTTP pipeline
+                        });
+
+                    webBuilder.UseStartup<Startup>();
+                });
+
+        return host.RunConsoleAsync();
+    }
+
+    sealed class MqttControllerVANL
+    {
+        public MqttControllerVANL()
+        {
+            // Inject other services via constructor.
+        }
+        
+        public Task OnClientConnected(ClientConnectedEventArgs eventArgs)
+        {
+            Console.WriteLine($"Client '{eventArgs.ClientId}' connected.");
+            return Task.CompletedTask;
+        }
+
+
+        public Task ValidateConnection(ValidatingConnectionEventArgs eventArgs)
+        {
+            Console.WriteLine($"Client '{eventArgs.ClientId}' wants to connect. Accepting!");
+            return Task.CompletedTask;
+        }
+
+        public Task OnClientDisconnected(ClientDisconnectedEventArgs eventArgs) 
+        {
+            var message = $"Client '{eventArgs.ClientId}' disconnected with type '{eventArgs.DisconnectType}' at '{DateTime.Now}'";
+            SendDiscordMs(discordWebhook, message);
+            
+            return Task.CompletedTask;
+        }
+        
+
+        static readonly string discordWebhook = "https://discord.com/api/webhooks/1192795355998330892/1R3Lic4souDscjofwWA7MrYFVcIYvKh9cutcatb8dagYfwIGKrgI84JFyEaZj90WnsDR";
+
+        static async void SendDiscordMs(string webhook, string message)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // Create JSON payload
+                string payload = $"{{\"content\": \"{message}\"}}";
+
+                // Send POST request
+                HttpResponseMessage response = await client.PostAsync(webhook, new StringContent(payload, System.Text.Encoding.UTF8, "application/json"));
+
+                // Check if the request was successful
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Message sent successfully");
+                }
+                else
+                {
+                    Console.WriteLine($"Error sending message. Status code: {response.StatusCode}");
+                }
+            }
+        }
+    }
+
+    sealed class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment environment, MqttControllerVANL mqttController)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(
+                endpoints =>
+                {
+                    endpoints.MapConnectionHandler<MqttConnectionHandler>(
+                        "/mqtt",
+                        httpConnectionDispatcherOptions => httpConnectionDispatcherOptions.WebSockets.SubProtocolSelector =
+                            protocolList => protocolList.FirstOrDefault() ?? string.Empty);
+                });
+
+            app.UseMqttServer(
+                server =>
+                {
+                    /*
+                     * Attach event handlers etc. if required.
+                     */
+
+                    server.ClientDisconnectedAsync += mqttController.OnClientDisconnected;
+                    server.ValidatingConnectionAsync += mqttController.ValidateConnection;
+                    server.ClientConnectedAsync += mqttController.OnClientConnected;
+                });
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHostedMqttServer(
+                optionsBuilder =>
+                {
+                    optionsBuilder.WithDefaultEndpoint();
+                });
+
+            services.AddMqttConnectionHandler();
+            services.AddConnections();
+
+            services.AddSingleton<MqttControllerVANL>();
+        }
+    }
+}

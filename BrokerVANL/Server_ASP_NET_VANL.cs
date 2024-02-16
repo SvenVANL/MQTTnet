9fcba88f77407a19333a7475add6c324ffbf03d5
SvenVANL <svanderpost@vanl.nl>
2024-02-16 12:59:24 +0100

    added credential validation + small improvements

--- BrokerVANL/Server_ASP_NET_VANL.cs
+++ BrokerVANL/Server_ASP_NET_VANL.cs
@@ -15,10 +15,72 @@ using Microsoft.Extensions.Hosting;
 using MQTTnet.AspNetCore;
 using MQTTnet.Server;
 
+using System.Text.RegularExpressions;
+
 namespace MQTTnet.BrokerVANL;
 
 public static class Server_ASP_NET_VANL
 {
+
+    private static readonly string UserName = "vanlclient";
+    private static readonly string Password = "theclown";
+
+    //Production webhook: "https://discord.com/api/webhooks/1192795355998330892/1R3Lic4souDscjofwWA7MrYFVcIYvKh9cutcatb8dagYfwIGKrgI84JFyEaZj90WnsDR";
+    private static readonly string discordWebhook = "https://discord.com/api/webhooks/1200433721686372574/BEcLrC_3OeAaFGnamonUyvC0s8RxlZSMRrZDCVutyyBe9qSGirJ-fj7MeL_agVrk7758"; 
+
+    static async void SendDiscordMs(string webhook, string message)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            // Create JSON payload
+            string payload = $"{{\"content\": \"{message}\"}}";
+
+            // Send POST request
+            HttpResponseMessage response = await client.PostAsync(webhook, new StringContent(payload, System.Text.Encoding.UTF8, "application/json"));
+
+            // Check if the request was successful
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Message sent successfully");
+            }
+            else
+            {
+                Console.WriteLine($"Error sending message. Status code: {response.StatusCode}");
+            }
+        }
+    }
+
+    static bool IsDashApp(string ClientId) 
+    {
+        // python dash app client
+        if (Regex.Match(ClientId, @"dash_mqtt_[a-zA-Z0-9]{8}").Success) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    static void LogClientConnected(ClientConnectedEventArgs eventArgs) 
+    {
+        if (!IsDashApp(eventArgs.ClientId)) 
+        {
+            var message = $"Client '{eventArgs.ClientId}' connected at '{DateTime.Now}'";
+            SendDiscordMs(discordWebhook, message);
+        }
+    }
+
+    static void LogClientDisconnected(ClientDisconnectedEventArgs eventArgs) 
+    {
+        if (!IsDashApp(eventArgs.ClientId)) 
+        {
+            var message = $"Client '{eventArgs.ClientId}' disconnected with type '{eventArgs.DisconnectType}' at '{DateTime.Now}'";
+            SendDiscordMs(discordWebhook, message);
+            
+            Console.WriteLine($"Client '{eventArgs.ClientId}' disconnected.");
+        }
+    }
+
     public static Task Start_Server_With_WebSockets_Support(System.Net.IPAddress address)
     {
         /*
@@ -57,49 +119,35 @@ public static class Server_ASP_NET_VANL
         
         public Task OnClientConnected(ClientConnectedEventArgs eventArgs)
         {
-            Console.WriteLine($"Client '{eventArgs.ClientId}' connected.");
+            LogClientConnected(eventArgs);
+
             return Task.CompletedTask;
         }
 
 
         public Task ValidateConnection(ValidatingConnectionEventArgs eventArgs)
         {
-            Console.WriteLine($"Client '{eventArgs.ClientId}' wants to connect. Accepting!");
+
+            if (eventArgs.UserName == UserName && eventArgs.Password == Password || IsDashApp(eventArgs.ClientId)) 
+            {
+                Console.WriteLine($"Client '{eventArgs.ClientId}' wants to connect with valid credentials. Accepting!");
+            }
+            else 
+            {
+                eventArgs.ReasonCode = Protocol.MqttConnectReasonCode.BadUserNameOrPassword;
+                Console.WriteLine($"Client '{eventArgs.ClientId}' wants to connect with invalid credentials. Denying!");
+            }            
+
             return Task.CompletedTask;
         }
 
         public Task OnClientDisconnected(ClientDisconnectedEventArgs eventArgs) 
         {
-            var message = $"Client '{eventArgs.ClientId}' disconnected with type '{eventArgs.DisconnectType}' at '{DateTime.Now}'";
-            SendDiscordMs(discordWebhook, message);
-            
+            LogClientDisconnected(eventArgs);
+
             return Task.CompletedTask;
         }
-        
-
-        static readonly string discordWebhook = "https://discord.com/api/webhooks/1192795355998330892/1R3Lic4souDscjofwWA7MrYFVcIYvKh9cutcatb8dagYfwIGKrgI84JFyEaZj90WnsDR";
-
-        static async void SendDiscordMs(string webhook, string message)
-        {
-            using (HttpClient client = new HttpClient())
-            {
-                // Create JSON payload
-                string payload = $"{{\"content\": \"{message}\"}}";
-
-                // Send POST request
-                HttpResponseMessage response = await client.PostAsync(webhook, new StringContent(payload, System.Text.Encoding.UTF8, "application/json"));
-
-                // Check if the request was successful
-                if (response.IsSuccessStatusCode)
-                {
-                    Console.WriteLine("Message sent successfully");
-                }
-                else
-                {
-                    Console.WriteLine($"Error sending message. Status code: {response.StatusCode}");
-                }
-            }
-        }
+                
     }
 
     sealed class Startup
